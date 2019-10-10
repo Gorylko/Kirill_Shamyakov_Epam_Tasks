b9535c8cc2774c3fa588b0d22ec44e2c5acbd1d7
gorylko <shamyachka@gmail.com>
2019-10-10 14:06:47 +0300

    added empty classes for task

---
+++ Module2/Tasks/Task1/Entities/ConsoleInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Interfaces;
+
+namespace Task1.Entities
+{
+    public class ConsoleInitializer : IInitializer
+    {
+        private int _companyCount;
+        private double _profit;
+        private double _taxPercentage;
+
+        public int GetCompanyCount()
+        {
+            return this._companyCount;
+        }
+
+        public double GetProfit()
+        {
+            return this._profit;
+        }
+
+        public double GetTaxPercentage()
+        {
+            return this._taxPercentage;
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Module2/Tasks/Task1/Interfaces/IInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Interfaces
+{
+    public interface IInitializer
+    {
+        void Initialize();
+
+        int GetCompanyCount();
+
+        double GetTaxPercentage();
+
+        double GetProfit();
+    }
+}
