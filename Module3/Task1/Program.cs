2c878119c78fc335cc4d159755e1f0f9e701131f
gorylko <shamyachka@gmail.com>
2019-10-18 17:39:36 +0300

    Task1 was done

--- Module3/Task1/Program.cs
+++ Module3/Task1/Program.cs
@@ -10,6 +10,32 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello! Enter two int numbers (press Enter after each)");
+
+            if (int.TryParse(Console.ReadLine(), out int firstNumber) &&
+                int.TryParse(Console.ReadLine(), out int secondNumber))
+            {
+                Console.WriteLine($"Result: {Multiply(firstNumber, secondNumber)}");
+            }
+            else
+            {
+                ReportBug();
+            }
+            Console.ReadKey();
+        }
+
+        private static int Multiply(int firstNum, int secondNum)
+        {
+            return (int)(firstNum / (1 / (double)secondNum)); //without multiplication 
+        }
+
+        static void ReportBug()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("User input isn't valid!!!");
+
+            Console.ResetColor();
         }
     }
 }
