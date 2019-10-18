9d24764e3ef3b6cae22793dc6b3e48369698a2c3
gorylko <shamyachka@gmail.com>
2019-10-19 00:17:41 +0300

    Task2 was done

---
+++ Module3/Task2/Extensions/CollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static string AsString(this IReadOnlyCollection<int> collection)
+        {
+            if(collection == null)
+            {
+                return string.Empty;
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var el in collection)
+            {
+                stringBuilder.AppendLine(el.ToString());
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- Module3/Task2/Program.cs
+++ Module3/Task2/Program.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task2.Extensions;
 
 namespace Task2
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter int number");
+
+            if(int.TryParse(Console.ReadLine(), out int number) &&
+                number > 0)
+            {
+                Console.Clear();
+                Console.WriteLine(GetFirstEvenNaturalNumbers(number).AsString());
+            }
+            else
+            {
+                BugReport();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static IReadOnlyCollection<int> GetFirstEvenNaturalNumbers(int amount)
+        {
+            if(amount <= 0)
+            {
+                return null;
+            }
+
+            var numberContainer = new List<int>();
+
+            for(int i = 0; i < amount; i++)
+            {
+                numberContainer.Add((i + 1) * 2);
+            }
+
+            return numberContainer;
+        }
+
+        private static void BugReport()
+        {
+            Console.WriteLine("User input is not valid");
         }
     }
 }
