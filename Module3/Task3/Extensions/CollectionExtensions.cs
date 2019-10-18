22f5d06e9e55112182d99c5ce5738c1cf7aaed93
gorylko <shamyachka@gmail.com>
2019-10-19 00:57:36 +0300

    Task3 was done

---
+++ Module3/Task3/Extensions/CollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static string AsString(this IEnumerable<int> collection)
+        {
+            if (collection == null)
+            {
+                return string.Empty;
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var el in collection)
+            {
+                stringBuilder.Append(el.ToString() + " ");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- Module3/Task3/Program.cs
+++ Module3/Task3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3.Extensions;
 
 namespace Task3
 {
@@ -10,6 +11,42 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter an integer greater than zero");
+            if (int.TryParse(Console.ReadLine(), out int number) &&
+                number > 0)
+            {
+                Console.Clear();
+                var result = GetFibonacciNums().TakeWhile((el, index) => index < number);
+
+                Console.WriteLine(result.AsString());
+
+            }
+            else
+
+
+            {
+                BugReport();
+            }
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> GetFibonacciNums()
+        {
+            int current = 0;
+            int next = 1;
+            while (true)
+            {
+                yield return current;
+                var temp = next;
+                next = current + next;
+                current = temp;
+            }
+        }
+
+        private static void BugReport()
+        {
+            Console.WriteLine("Invalid user input");
         }
     }
 }
