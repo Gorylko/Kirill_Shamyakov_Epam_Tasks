cfe9ce79a6f8c59d3b0f3c574fcddb706899d939
gorylko <shamyachka@gmail.com>
2019-10-19 11:57:48 +0300

    Task4 was done

---
+++ Module3/Task4/NumberReverser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class NumberReverser
+    {
+        private bool _isNegative;
+
+        public void Reverse(ref int number)
+        {
+            if(number == 0)
+            {
+                return;
+            }
+
+            if (number < 0)
+            {
+                _isNegative = true;
+                number *= -1;
+            }
+
+            number = this._isNegative ? - int.Parse(new string(number.ToString().ToCharArray().Reverse().ToArray()))
+                : int.Parse(new string(number.ToString().ToCharArray().Reverse().ToArray()));
+            _isNegative = false;
+        }
+
+        public void Reverse(ref double number)
+        {
+            if (number == 0)
+            {
+                return;
+            }
+
+            if (number < 0)
+            {
+                _isNegative = true;
+                number *= -1;
+            }
+
+            number = this._isNegative? - double.Parse(new string(number.ToString().ToCharArray().Reverse().ToArray()))
+                : double.Parse(new string(number.ToString().ToCharArray().Reverse().ToArray()));
+            _isNegative = false;
+        }
+    }
+}
--- Module3/Task4/Program.cs
+++ Module3/Task4/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Task4
 {
@@ -10,6 +9,57 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            var reverser = new NumberReverser();
+
+            while (true)
+            {
+                Console.WriteLine("Enter the number");
+
+                var userString = Console.ReadLine();
+
+                if (IsValidForDoubleParsing(userString))
+                {
+
+                    if (double.TryParse(userString, out double parsedDouble))
+                    {
+                        reverser.Reverse(ref parsedDouble);
+                        Console.WriteLine(parsedDouble);
+                    }
+                }
+                else if (int.TryParse(userString, out int parsedInt))
+                {
+                    reverser.Reverse(ref parsedInt);
+                    Console.WriteLine(parsedInt);
+                }
+                else
+                {
+                    BugReport("Input is not a number");
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private static bool IsValidForDoubleParsing(string stringDouble)
+        {
+            if (stringDouble == null
+                || stringDouble == ""
+                || stringDouble.Contains("-,")
+                || stringDouble.Contains("-."))
+            {
+                return false;
+            }
+
+            return stringDouble.Where(el => el == ',' || el == '.').Count() == 1 &&
+                    !".,".Contains(stringDouble[stringDouble.Length - 1]) &&
+                    (!".,".Contains(stringDouble[0]) || !(stringDouble[0] != '-'));
+        }
+
+        private static void BugReport(string additionalInformation)
+        {
+            Console.WriteLine("An application execution error has occurred");
+            Console.WriteLine($"Additional Information : {additionalInformation}");
         }
     }
 }
+
