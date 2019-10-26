ebb89b7dff7424770604c260e399f45b95372372
gorylko <shamyachka@gmail.com>
2019-10-26 13:52:19 +0300

    overload for GetOperation method was added

--- Module4/Task5/Calculator.cs
+++ Module4/Task5/Calculator.cs
@@ -45,6 +45,45 @@ namespace Task5
             }
         }
 
+        public CountingResult<double> GetOperationResult(double firstNumber, double secondNumber, MathOperation operationType)
+        {
+            switch (operationType)
+            {
+                case MathOperation.Multiplication:
+                    return new CountingResult<double>
+                    {
+                        Params = firstNumber * secondNumber,
+                        IsSuccessful = true
+                    };
+
+                case MathOperation.Division:
+                    return secondNumber == 0 ?
+                        GetErrorResult<double>($"Invalid value of {nameof(secondNumber)}, the value of the number was 0") :
+                        new CountingResult<double>
+                        {
+                            Params = firstNumber / secondNumber,
+                            IsSuccessful = true
+                        };
+
+                case MathOperation.Sum:
+                    return new CountingResult<double>
+                    {
+                        Params = firstNumber + secondNumber,
+                        IsSuccessful = true
+                    };
+
+                case MathOperation.Difference:
+                    return new CountingResult<double>
+                    {
+                        Params = firstNumber - secondNumber,
+                        IsSuccessful = true
+                    };
+
+                default:
+                    return GetErrorResult<double>("It's impossible!!! Hit the programmer on the head if you see it");
+            }
+        }
+
         private CountingResult<T>GetErrorResult<T>(string errorMessage)
         {
             return new CountingResult<T>
