4df126458ed6446a452e1f5ef07550388465a0d3
gorylko <shamyachka@gmail.com>
2019-10-26 14:10:24 +0300

    added methods for task 6

---
+++ Module4/Task6/CollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task6
+{
+    public static class CollectionExtensions
+    {
+        private static void IncreaseAllNumbers(this IEnumerable<int> numberCollection, int amount)
+        {
+            numberCollection = numberCollection.Select(el => el + amount);
+        }
+
+        private static void IncreaseAllNumbers(this IEnumerable<double> numberCollection, double amount)
+        {
+            numberCollection = numberCollection.Select(el => el + amount);
+        }
+    }
+}
