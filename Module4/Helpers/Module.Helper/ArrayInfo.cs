85f4900f6981bebe3ad0c7694dfcdb163e747065
gorylko <shamyachka@gmail.com>
2019-10-26 12:51:28 +0300

    Added methodPacks of Task3-4

---
+++ Module4/Helpers/Module.Helper/ArrayInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Module.Helper
+{
+    public struct ArrayInfo<T> where T : struct
+    {
+        public T MaxElement { get; set; }
+
+        public T MinElement { get; set; }
+
+        public T SumOfAllElements { get; set; }
+    }
+}
