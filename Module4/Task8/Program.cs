6995eb25cccb4acba2f845913634b1a14ebbd0eb
gorylko <shamyachka@gmail.com>
2019-10-27 12:07:55 +0300

    Task8 was done

--- Module4/Task8/Program.cs
+++ Module4/Task8/Program.cs
@@ -10,6 +10,24 @@ namespace Task8
     {
         static void Main(string[] args)
         {
+            Func<double, double> function = x => 12 * x * x + 15 * x - 7;
+
+            var provider = new EquationCalculator()
+            {
+                Function = function,
+                LeftBorder = -20,
+                RightBorder = 20,
+                Accuracy = 0.001
+            };
+
+            var providerResult = provider.FindZero();
+
+            if (providerResult.IsSuccessful)
+            {
+                Console.WriteLine($"x = {providerResult.Params}; f(x) = {function(providerResult.Params)}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
