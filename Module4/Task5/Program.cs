741de84cbab72d9739dd95ececa3c69f08f80a2f
gorylko <shamyachka@gmail.com>
2019-10-31 20:12:32 +0300

    added preview code to program.cs

--- Module4/Task5/Program.cs
+++ Module4/Task5/Program.cs
@@ -20,10 +20,20 @@ namespace Task5
                     secondResult.IsSuccessful)
                 {
                     var calculator = new Calculator();
-                    Console.WriteLine();
+
+                    var multResult = calculator.GetOperationResult(firstResult.Value, secondResult.Value, MathOperation.Multiplication);
+                    var divResult = calculator.GetOperationResult(firstResult.Value, secondResult.Value, MathOperation.Division);
+                    var sumResult = calculator.GetOperationResult(firstResult.Value, secondResult.Value, MathOperation.Sum);
+                    var difResult = calculator.GetOperationResult(firstResult.Value, secondResult.Value, MathOperation.Difference);
+
+                    Console.WriteLine($"Multiplication : {multResult.Params}");
+                    Console.WriteLine($"Division : {(divResult.IsSuccessful ? divResult.Params.ToString() : "Can not be divided by zero")}");
+                    Console.WriteLine($"Sum : {sumResult.Params}");
+                    Console.WriteLine($"Difference : {difResult.Params}");
                 }
             } while (!isValidInput);
 
+            Console.WriteLine($"Days in the current month : {GetDaysInMonth(DateTime.Now.Year, DateTime.Now.Month)}");
             Console.ReadKey();
         }
 
