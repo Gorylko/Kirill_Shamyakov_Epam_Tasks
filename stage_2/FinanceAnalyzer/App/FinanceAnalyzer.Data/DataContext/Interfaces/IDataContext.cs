b4398d18a3c4a42f7dd0caa3fe826290a30f7f4f
gorylko <shamyachka@gmail.com>
2019-12-05 13:03:46 +0300

    references + data context interfaces was added

---
+++ stage_2/FinanceAnalyzer/App/FinanceAnalyzer.Data/DataContext/Interfaces/IDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceAnalyzer.Data.DataContext.Interfaces
+{
+    public interface IDataContext<T>
+    {
+        T GetByUserId(int userId);
+
+        void Save(T obj);
+
+        void Delete(T obj);
+    }
+}
